c862e1708dd22da5fe65c46ec7701d4e21c81aa5
Dan Smith <dev@dotherightthing.co.nz>
2018-05-28 10:57:11 +1200

    Fixes for PHPCS

--- template-parts/wpdtrt-anchorlinks/content-anchorlinks.php
+++ template-parts/wpdtrt-anchorlinks/content-anchorlinks.php
@@ -4,7 +4,7 @@
  *
  * @package   DTRT Anchor Links
  * @version   0.0.1
- * @since     0.7.0
+ * @since     0.7.0 DTRT WordPress Plugin Boilerplate Generator
  */
 
 // Predeclare variables
@@ -12,13 +12,10 @@
 // Internal WordPress arguments available to widgets
 // This allows us to use the same template for shortcodes and front-end widgets
 $before_widget = null; // register_sidebar
-$before_title = null; // register_sidebar
-$title = null;
-$after_title = null; // register_sidebar
-$after_widget = null; // register_sidebar
-
-// shortcode options
-// $foo = null;
+$before_title  = null; // register_sidebar
+$title         = null;
+$after_title   = null; // register_sidebar
+$after_widget  = null; // register_sidebar
 
 // access to plugin
 $plugin = null;
@@ -30,22 +27,18 @@ $options = get_query_var( 'options' );
 // @link http://kb.network.dan/php/wordpress/extract/
 extract( $options, EXTR_IF_EXISTS );
 
-// load the data
-// $plugin->get_api_data();
-// $foo = $plugin->get_api_data_bar();
-
 // WordPress widget options (not output with shortcode)
 echo $before_widget;
 echo $before_title . $title . $after_title;
 ?>
 
 <div class="wpdtrt-anchorlinks">
-  <?php
-    /* ====== Add plugin output here ====== */
-  ?>
+	<?php
+		/* ====== Add plugin output here ====== */
+	?>
 </div>
 
 <?php
-  // output widget customisations (not output with shortcode)
-  echo $after_widget;
+// output widget customisations (not output with shortcode)
+echo $after_widget;
 ?>
