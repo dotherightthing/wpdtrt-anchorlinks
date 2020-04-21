c2502a2404c788127844a3bf455ed1fd976a84e1
Dan Smith <dev@dotherightthing.co.nz>
2020-04-21 14:31:42 +1200

    Add file required for build

---
+++ js/backend.js
@@ -0,0 +1,7 @@
+/**
+ * @file DTRT Anchor Links backend.js
+ * @summary
+ *     Back-end scripting for public pages
+ * @version 0.0.1
+ * @see 0.7.0 DTRT WordPress Plugin Boilerplate Generator
+ */
