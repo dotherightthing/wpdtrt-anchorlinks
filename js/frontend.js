7b0353232ad3ffcd9e3ede9dc94e1f43fa9f0d92
Dan Smith <dev@dotherightthing.co.nz>
2020-04-21 14:20:21 +1200

    Lint JS

--- js/frontend.js
+++ js/frontend.js
@@ -1,10 +1,10 @@
 /**
- * @file DTRT Tour dates frontend.js
+ * @file DTRT Anchor Links frontend.js
  * @summary
  *     Front-end scripting for public pages
  *     PHP variables are provided in `wpdtrtAnchorlinksConfig`.
  * @version 0.0.1
- * @since 0.7.0 DTRT WordPress Plugin Boilerplate Generator
+ * @see 0.7.0 DTRT WordPress Plugin Boilerplate Generator
  */
 
 /* eslint-env browser */
@@ -12,10 +12,25 @@
 /* eslint-disable no-unused-vars, max-len, require-jsdoc */
 
 /**
+ * jQuery object
+ *
+ * @external jQuery
+ * @see {@link http://api.jquery.com/jQuery/}
+ */
+
+/**
  * @namespace wpdtrtAnchorlinksUi
  */
 const wpdtrtAnchorlinksUi = {
 
+    /**
+     * @function sticky_jump_menu
+     * @summary Inject wrappers required for fixed positioning, manage scroll link highlighting
+     * @memberof wpdtrtAnchorlinksUi
+     * @protected
+     *
+     * @param {external:jQuery} $ - jQuery
+     */
     sticky_jump_menu: ($) => {
         const $jumpMenu = $('.mwm-aal-container');
 
@@ -61,11 +76,25 @@ const wpdtrtAnchorlinksUi = {
          * + DTRT make sticky
          */
 
-        // Get link by section or article id
+        /**
+         * @function getRelatedNavigation
+         * @summary Get link by section or article id
+         * @memberof wpdtrtAnchorlinksUi
+         * @protected
+         *
+         * @param {external:jQuery} el - anchor element
+         * @returns {external:jQuery} link element
+         */
         function getRelatedNavigation(el) {
             return $(`.mwm-aal-container a[href='#${$(el).attr('id')}']`);
         }
 
+        /**
+         * @function showScrollProgress
+         * @summary Resize the indicator according to the scroll progress
+         * @memberof wpdtrtAnchorlinksUi
+         * @protected
+         */
         function showScrollProgress() {
             const $mwmTitle = $('.mwm-aal-title');
             const $mwmLinks = $('.mwm-aal-container > ol a');
@@ -88,13 +117,13 @@ const wpdtrtAnchorlinksUi = {
             $('.scroll-progress').css('width', `${pctThru}%`);
         }
 
-        if ($('section.scrollto').length) {
+        if ($('.wpdtrt-anchorlinks__anchor').length) {
             const sticky = new Waypoint.Sticky({
                 element: $jumpMenuLayout[0],
                 stuckClass: 'sticky'
             });
 
-            $('section.scrollto').waypoint(
+            $('.wpdtrt-anchorlinks__anchor').waypoint(
                 function (direction) { // eslint-disable-line func-names
                     // Highlight element when related content
                     // is 10% percent from the bottom...
@@ -107,7 +136,7 @@ const wpdtrtAnchorlinksUi = {
                 }
             );
 
-            $('section.scrollto').waypoint(
+            $('.wpdtrt-anchorlinks__anchor').waypoint(
                 function (direction) { // eslint-disable-line func-names
                     // Highlight element when bottom of related content
                     // is 100px from the top - remove if less
@@ -138,6 +167,14 @@ const wpdtrtAnchorlinksUi = {
         }
     },
 
+    /**
+     * @function showScrollProgress
+     * @summary Initialise the component
+     * @memberof wpdtrtAnchorlinksUi
+     * @public
+     *
+     * @param {external:jQuery} $ - jQuery
+     */
     init: ($) => { // called from footer config script block
         // https://web-design-weekly.com/snippets/scroll-to-position-with-jquery/
         $.fn.scrollView = function (offset, duration) { // eslint-disable-line func-names
